32b63c63da4eb56c494138d34c7d2eeff65fe507
hristiyan-anchev <hristian.anchev@gmail.com>
2021-02-23 14:11:07 +0000

    added some security configurations

---
+++ src/main/java/socialnet/configuration/SecurityConfig.java
@@ -0,0 +1,62 @@
+package socialnet.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+
+    private final UserDetailsService userDetailsService;
+    private final PasswordEncoder passwordEncoder;
+
+    public SecurityConfig(UserDetailsService userDetailsService, PasswordEncoder passwordEncoder) {
+        this.userDetailsService = userDetailsService;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth
+                .userDetailsService(userDetailsService)
+                .passwordEncoder(passwordEncoder);
+    }
+
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+//                .cors().disable()
+                .csrf().disable()
+                .authorizeRequests()
+                // add more specific routes at the top
+
+
+                .antMatchers("/js/**", "/css/**", "/image/**").permitAll()
+                .antMatchers("/").permitAll()
+
+//                .anyRequest().authenticated()
+//                .and()
+//                .formLogin()
+//                .loginPage("/users/login")
+//                .usernameParameter("username")
+//                .passwordParameter("password")
+//                .defaultSuccessUrl("/")
+//                .failureUrl("/users/login?error=true")
+//                .and()
+//                .logout()
+//                .logoutUrl("/users/logout")
+//                .logoutSuccessUrl("/")
+//        .deleteCookies()
+
+        ;
+
+    }
+}
