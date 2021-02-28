e6d48a5041dbbadee28641b490ce877414f9da6f
lamarin1 <marin.popov@gmail.com>
2021-02-28 15:43:04 +0200

    Get all messages

--- src/main/java/socialnet/repository/MessageRepository.java
+++ src/main/java/socialnet/repository/MessageRepository.java
@@ -2,14 +2,17 @@ package socialnet.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import socialnet.models.entities.Message;
+import socialnet.models.entities.User;
+import socialnet.models.servicies.MessageServiceModel;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface MessageRepository extends JpaRepository<Message, String> {
 
     Optional<Message> getAllByAuthor(String username);
 
-    Optional<Message> getAllByRecipient(String recipient);
+    List<MessageServiceModel> getAllByRecipient(User recipient);
 
     //TODO
 }
--- src/main/java/socialnet/service/impl/MessageServiceImpl.java
+++ src/main/java/socialnet/service/impl/MessageServiceImpl.java
@@ -1,8 +1,12 @@
 package socialnet.service.impl;
 
 import org.springframework.stereotype.Service;
+import socialnet.models.entities.User;
+import socialnet.models.servicies.MessageServiceModel;
 import socialnet.repository.MessageRepository;
 import socialnet.service.MessageService;
+
+import java.util.List;
 
 @Service
 public class MessageServiceImpl implements MessageService {
@@ -11,5 +15,21 @@ public class MessageServiceImpl implements MessageService {
 
     public MessageServiceImpl(MessageRepository messageRepository) {
         this.messageRepository = messageRepository;
+    }
+
+    @Override
+    public MessageServiceModel send(MessageServiceModel message) {
+        return null;
+    }
+
+    @Override
+    public List<MessageServiceModel> getAllByRecipient(User recipient) {
+        return this.messageRepository.getAllByRecipient(recipient);
+    }
+
+
+    @Override
+    public List<MessageServiceModel> getAllByStatus(MessageServiceModel status) {
+        return null;
     }
 }
