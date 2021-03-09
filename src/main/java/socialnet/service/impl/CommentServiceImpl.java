26e425b2c00e621a9b523f1cf0d4cc38925449de
Ivaylo Ivanov <69511821+ivaka4@users.noreply.github.com>
2021-03-09 13:29:10 +0200

    Added logic in CommentService and changed Comment Entity

--- src/main/java/socialnet/service/impl/CommentServiceImpl.java
+++ src/main/java/socialnet/service/impl/CommentServiceImpl.java
@@ -1,25 +1,65 @@
 package socialnet.service.impl;
 
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
+import socialnet.exceptions.CommentException;
+import socialnet.models.entities.Comment;
+import socialnet.models.entities.User;
 import socialnet.models.servicies.CommentServiceModel;
+import socialnet.repository.CommentRepository;
+import socialnet.repository.UserRepository;
 import socialnet.service.CommentService;
+
+import java.time.LocalDateTime;
+import java.util.Optional;
 
 @Service
 public class CommentServiceImpl implements CommentService {
-// TODO
+    private final CommentRepository commentRepository;
+    private final ModelMapper modelMapper;
+    private final UserRepository userRepository;
+
+    @Autowired
+    public CommentServiceImpl(CommentRepository commentRepository, ModelMapper modelMapper, UserRepository userRepository) {
+        this.commentRepository = commentRepository;
+        this.modelMapper = modelMapper;
+        this.userRepository = userRepository;
+    }
+
     @Override
     public CommentServiceModel addComment(CommentServiceModel commentServiceModel) {
-        return null;
+        Comment comment = this.modelMapper.map(commentServiceModel, Comment.class);
+        User author = this.modelMapper.map(commentServiceModel.getAuthor(), User.class);
+        if (author == null){
+            throw new UsernameNotFoundException("Cannot find user");
+        }
+        comment.setAuthor(author);
+        this.commentRepository.saveAndFlush(comment);
+        return this.modelMapper.map(comment, CommentServiceModel.class);
     }
 
     @Override
-    public boolean deleteComment(String id, String username) {
-        return false;
+    public boolean deleteComment(String id, String username) throws CommentException {
+       Comment comment = this.commentRepository.findById(id).orElse(null);
+       if (comment == null){
+           throw new CommentException("Cannot find comment");
+       }
+       this.commentRepository.delete(comment);
+        return true;
     }
 
     @Override
-    public CommentServiceModel editComment(CommentServiceModel commentServiceModel) {
-        return null;
+    public CommentServiceModel editComment(CommentServiceModel commentServiceModel) throws CommentException {
+        Comment comment = this.commentRepository.findById(commentServiceModel.getId()).orElse(null);
+        if (comment == null){
+            throw new CommentException("Cannot edit comment");
+        }
+        comment.setContent(commentServiceModel.getContent());
+        comment.setUpdatedOn(LocalDateTime.now());
+        this.commentRepository.saveAndFlush(comment);
+        return this.modelMapper.map(comment, CommentServiceModel.class);
     }
 }
