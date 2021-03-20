6732f76b1abd41a9972bb64a53be6a17713ef1dc
zstefchev <zstefchev@gmail.com>
2021-03-20 12:36:40 +0100

    .

---
+++ src/main/java/socialnet/service/JwtTokenProvider.java
@@ -0,0 +1,31 @@
+package socialnet.service;
+
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+
+import socialnet.models.servicies.UserServiceModel;
+
+interface JwtTokenProvider {
+
+	String createJwtToken(UserServiceModel usm);
+	
+	List<GrantedAuthority> getAuthoritiesFromToken(String token);
+	
+	boolean isTokenValid(String token, String username);
+	
+	String getSubjectFromToken(String token);
+	
+	Authentication getAuthentication(String username, List<GrantedAuthority> authorities,HttpServletRequest request);
+
+//	boolean isTokenExpired(String token, JWTVerifier jwtVerifier);
+
+	String[] getClaimsFromUser(UserServiceModel usm);
+	
+	String[] getClaimsFromToken(String token);
+	
+//	JWTVerifier getJWTVerifier();
+}
